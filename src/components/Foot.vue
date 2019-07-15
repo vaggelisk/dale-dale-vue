86143f1af02a8aa8f738b63752cc514e1e9f92f7
Vagelis Karakaxis <vkarakaxis@obor-sinobridge.com>
2019-07-15 13:55:02 +0300

    foot topBar and more

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,25 +1,23 @@
 <template>
   <footer>
-    Made by <a href="https://macarthur.me">Alex MacArthur</a>
-    //
-    See <a href="https://www.github.com/alexmacarthur/wp-vue">WP Vue on Github</a>.
+    Made by proletarians for purpose of class antisexist war
   </footer>
 </template>
 
 <script>
-  export default {
-    name: 'Foot'
-  }
+export default {
+  name: "Foot"
+};
 </script>
 
 <style scoped lang="scss">
-  footer {
-    text-align: center;
-    padding: 1rem;
-    background: $gray--light;
-  }
+footer {
+  text-align: center;
+  padding: 1rem;
+  background: $gray--light;
+}
 
-  a {
-    font-weight: bold;
-  }
+a {
+  font-weight: bold;
+}
 </style>
--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,70 +1,66 @@
 <template>
   <div>
-
     <section>
       <p>
-        WP Vue is a simple template built with Vue JS that displays posts from a WordPress REST API endpoint.
-        Take what you see here &amp; rip it apart to suit your needs. To improve it for everyone else, <a href="https://www.github.com/alexmacarthur/wp-vue">contribute on Github</a>.
+        Για την όξυνση του ταξικού ανταγωνισμού εντός της άθλησης.
+      </p>
+      <p>
+        Για την άμβλυνση του αθλητικού ανταγωνισμού εντός της τάξης.
       </p>
     </section>
 
     <ul>
-      <Card
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-      />
+      <Card v-for="post in posts" :post="post" :key="post.id" />
     </ul>
 
     <Pagination
       :currentPage="parseInt(page)"
       :totalPages="parseInt(totalPages)"
     ></Pagination>
-
   </div>
 </template>
 
 <script>
-import bus from '../bus';
-import ajax from '../mixins/ajax';
-import Card from '../components/Card';
-import Pagination from '../components/Pagination';
+import bus from "../bus";
+import ajax from "../mixins/ajax";
+import Card from "../components/Card";
+import Pagination from "../components/Pagination";
 
 export default {
-  name: 'Feed',
+  name: "Feed",
 
   mixins: [ajax],
 
-  data () {
+  data() {
     return {
       posts: [],
       page: 1,
       totalPages: null
-    }
+    };
   },
 
-  mounted: function () {
+  mounted: function() {
     this.getPosts();
   },
 
-  created: function () {
-    if(this.$route.name === 'page') {
+  created: function() {
+    if (this.$route.name === "page") {
       this.page = this.$route.params.page;
     }
   },
 
   methods: {
-    getPosts: async function () {
+    getPosts: async function() {
       let response;
 
       try {
         response = await this.get(
           `/posts?per_page=${POSTS_PER_PAGE}&page=${this.page}`
         );
-        this.totalPages = response.headers['x-wp-totalpages'];
+        this.totalPages = response.headers["x-wp-totalpages"];
       } catch (error) {
-        bus.$emit('showUpdater', 'Are you sure that\'s a valid endpoint?');
-        bus.$emit('toggleLoading', false);
+        bus.$emit("showUpdater", "Are you sure that's a valid endpoint?");
+        bus.$emit("toggleLoading", false);
         return;
       }
 
@@ -72,18 +68,16 @@ export default {
 
       this.getAdjacentPageData();
 
-      bus.$emit('toggleLoading', false);
+      bus.$emit("toggleLoading", false);
     },
 
-    getAdjacentPageData: async function (prevPage = false) {
-
-      let page = prevPage === true
-              ? parseInt(this.page) - 1
-              : parseInt(this.page) + 1;
+    getAdjacentPageData: async function(prevPage = false) {
+      let page =
+        prevPage === true ? parseInt(this.page) - 1 : parseInt(this.page) + 1;
 
       let response;
 
-      if(page > 0) {
+      if (page > 0) {
         try {
           response = await this.get(
             `/posts?per_page=${POSTS_PER_PAGE}&page=${page}`
@@ -92,33 +86,31 @@ export default {
           console.error(error);
         }
 
-        response.data.forEach((post) => {
-          if(post.featured_media <= 0) return;
+        response.data.forEach(post => {
+          if (post.featured_media <= 0) return;
           this.get(`/media/${post.featured_media}`);
         });
       }
 
-      if(!prevPage) {
+      if (!prevPage) {
         this.getAdjacentPageData(true);
       }
     },
 
-    getFeaturedImages: function (posts) {
-      return new Promise((resolve) => {
-        let requests = posts.map((post) => {
-          return new Promise( async (resolve) => {
+    getFeaturedImages: function(posts) {
+      return new Promise(resolve => {
+        let requests = posts.map(post => {
+          return new Promise(async resolve => {
             let response;
 
             try {
-
-              if(post.featured_media <= 0) {
+              if (post.featured_media <= 0) {
                 throw "No featured image.";
               }
 
-              response = await this.get(
-                `/media/${post.featured_media}`
-              );
-              post.featured_image = response.data.media_details.sizes['medium'].source_url;
+              response = await this.get(`/media/${post.featured_media}`);
+              post.featured_image =
+                response.data.media_details.sizes["medium"].source_url;
             } catch (error) {
               post.featured_image = null;
             }
@@ -127,7 +119,7 @@ export default {
           });
         });
 
-        Promise.all(requests).then((posts) => resolve(posts));
+        Promise.all(requests).then(posts => resolve(posts));
       });
     }
   },
@@ -136,22 +128,19 @@ export default {
     Card,
     Pagination
   }
-}
-
+};
 </script>
 
 <style scoped lang="scss">
+section {
+  text-align: center;
+  max-width: 800px;
+  margin: 0 auto 3rem;
+}
 
-  section {
-    text-align: center;
-    max-width: 800px;
-    margin: 0 auto 3rem;
-  }
-
-  ul {
-    display: grid;
-    grid-template-columns: repeat( auto-fit, minmax( 320px, auto ) );
-    grid-gap: 1rem;
-  }
+ul {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, auto));
+  grid-gap: 1rem;
+}
 </style>
-
